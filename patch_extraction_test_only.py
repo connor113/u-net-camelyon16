1ace1e8007d7a9f100450cc736efb8848f694815
Connor Atkins <2005348@swansea.ac.uk>
2023-11-02 13:11:59 +0000

    test

---
+++ patch_extraction_test_only.py
@@ -0,0 +1,56 @@
+import os
+from tqdm import tqdm
+# Get the current working directory
+current_path = os.getcwd()
+# Define the relative paths to your data and .venv folders
+venv_path = os.path.join(current_path, '.venv')
+
+# Use the relative paths in your code
+OPENSLIDE_PATH = os.path.join(venv_path, 'Lib', 'site-packages', 'openslide-win64-20230414', 'bin')
+
+if hasattr(os, 'add_dll_directory'):
+    with os.add_dll_directory(OPENSLIDE_PATH):
+        import openslide
+else:
+    import openslide
+from src.preprocessing import extract_and_save_patches_and_labels
+
+def main():
+    data_root = os.path.join(current_path, 'data')
+    raw_root = os.path.join(data_root, 'raw')
+    annotations_root = os.path.join(data_root, 'annotations')
+    preprocessed_patches_root = os.path.join(data_root, 'preprocessed_patches')
+    
+    # Handle test set
+    tqdm.write("Processing test set...")
+    test_folder = os.path.join(raw_root, 'test')
+    if os.path.exists(test_folder):
+        process_test_slides(test_folder, 'test', annotations_root, preprocessed_patches_root)
+
+
+def process_test_slides(test_folder, subset, annotations_root, preprocessed_patches_root):
+    for slide in tqdm(os.listdir(test_folder)):
+        slide_path = os.path.join(test_folder, slide)
+        
+        # Determine if there's an associated annotation
+        annotation_path = None
+        annotation_file = slide.replace('.tif', '.xml')  # Assuming .tif and .xml extensions
+        annotation_path_candidate = os.path.join(annotations_root, subset, annotation_file)
+        
+        if os.path.exists(annotation_path_candidate):
+            annotation_path = annotation_path_candidate
+        
+        # Define the save path
+        save_folder = os.path.join(preprocessed_patches_root, subset)
+        os.makedirs(save_folder, exist_ok=True)
+        save_path = os.path.join(save_folder, slide.replace('.tif', ''))
+        
+        # Call your patch extraction function
+        extract_and_save_patches_and_labels(
+            slide_path, save_path, tissue_threshold=0.3,  # other parameters here
+            annotation_path=annotation_path, patch_size=(512, 512), stride=(512, 512),
+            enable_logging=True
+        )
+
+if __name__ == '__main__':
+    main()
--- src/camelyon_dataloader.py
+++ src/camelyon_dataloader.py
@@ -4,4 +4,41 @@ import h5py
 import os
 
 class CamelyonDataset(Dataset):
-    def __init__
+    def __init__(self, root_dir, train=True, transforms=None):
+        """
+        Args:
+            root_dir (string): Directory with all the H5 files.
+            train (bool, optional): If True, use training set. Use test set otherwise.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.root_dir = os.path.join(root_dir, 'train' if train else 'test')
+        self.transforms = transforms
+        self.sample_counts = self.count_samples_in_hdf5()
+
+    def count_samples_in_hdf5(self):
+        """
+        Count the number of samples in HDF5 files within the root directory of the dataset.
+
+        Returns:
+            int: The total number of samples across all HDF5 files.
+        """
+        total_samples = 0
+        # Iterate over all HDF5 files in the root directory
+        for hdf5_filename in os.listdir(self.root_dir):
+            hdf5_filepath = os.path.join(self.root_dir, hdf5_filename)
+            if hdf5_filepath.endswith('.h5'):
+                with h5py.File(hdf5_filepath, 'r') as f:
+                    # Assuming the main group is named after the slide without the '.h5' extension
+                    slide_group_name = os.path.splitext(hdf5_filename)[0]
+                    if slide_group_name in f:
+                        level_group = f[slide_group_name].get(f"Level_0")
+                        if level_group:
+                            patch_group = level_group.get("patches")
+                            if patch_group:
+                                total_samples += len(patch_group)
+        return total_samples
+
+    def __len__(self):
+        return self.sample_counts
+    
+    
